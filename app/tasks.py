142db2163a0355d46faee7810ecca1bf5a9e56af
Shelton Gayles <sgbythelake@gmail.com>
2020-03-08 17:35:10 -0700

    added image resizing tazk

--- app/tasks.py
+++ app/tasks.py
@@ -1,7 +1,9 @@
+from PIL import Image
 from urllib import request
 from bs4 import BeautifulSoup
 import lxml
 import time
+import os
 
 
 def count_words(url):
@@ -32,3 +34,59 @@ def count_words(url):
     print(f"Time elapsed: {time_elapsed}")
 
     return len(word_count)
+
+
+# Create Image Set
+
+
+def create_image_set(image_dir, image_name):
+
+    start = time.time()
+
+    thumb = 50, 50
+    small = 540, 540
+    medium = 768, 768
+    large = 1080, 1080
+    xl = 1200, 1200
+
+    image = Image.open(os.path.join(image_dir, image_name))
+    image_ext = image_name.split(".")[-1]
+    image_name = image_name.split(".")[0]
+
+    # Thumbnail
+    thumbnail_image = image.copy()
+    thumbnail_image.thumbnail(thumb, Image.LANCZOS)
+    thumbnail_image.save(
+        f"{os.path.join(image_dir, image_name)}-thumbnail.{image_ext}", optimize=True, quality=95)
+
+    # Small
+    small_image = image.copy()
+    small_image.thumbnail(small, Image.LANCZOS)
+    small_image.save(
+        f"{os.path.join(image_dir, image_name)}-540.{image_ext}", optimize=True, quality=95)
+
+    # Medium
+    medium_image = image.copy()
+    medium_image.thumbnail(medium, Image.LANCZOS)
+    medium_image.save(
+        f"{os.path.join(image_dir, image_name)}-768.{image_ext}", optimize=True, quality=95)
+
+    # Large
+    large_image = image.copy()
+    large_image.thumbnail(large, Image.LANCZOS)
+    large_image.save(
+        f"{os.path.join(image_dir, image_name)}-1080.{image_ext}", optimize=True, quality=95)
+
+    # XL
+    xl_image = image.copy()
+    xl_image.thumbnail(xl, Image.LANCZOS)
+    xl_image.save(
+        f"{os.path.join(image_dir, image_name)}-1200.{image_ext}", optimize=True, quality=95)
+
+    end = time.time()
+
+    time_elapsed = end - start
+
+    print(f"Task complete in: {time_elapsed}")
+
+    return True
--- app/views.py
+++ app/views.py
@@ -1,11 +1,16 @@
+from app.tasks import create_image_set
 from app import app
 from app.tasks import count_words
 from app import r
 from app import q
 
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, flash
 
 from time import strftime
+import os
+import secrets
+app.config['SECRET_KEY'] = 'sfsfsecra24545sfac'
+app.config['UPLOAD_DIR'] = '/Users/sheltong/Desktop/julian_webscrape/app/static/img/uploads'
 
 
 @app.route('/')
@@ -30,3 +35,29 @@ def add_task():
         message = f"Task queued at {task.enqueued_at.strftime('%a %d %b %Y %H:%M:%S')}. {q.len} jobs queued"
 
     return render_template('add_task.html', message=message, jobs=jobs)
+
+
+@app.route('/upload-image', methods=["GET", "POST"])
+def upload_image():
+
+    message = None
+
+    if request.method == "POST":
+        image = request.files["image"]
+        image_dir_name = secrets.token_hex(16)
+        os.mkdir(os.path.join(app.config["UPLOAD_DIR"], image_dir_name))
+
+        image.save(os.path.join(
+            app.config["UPLOAD_DIR"], image_dir_name, image.filename))
+        image_dir = os.path.join(
+            app.config["UPLOAD_DIR"], image_dir_name)
+        q.enqueue(create_image_set, image_dir, image.filename)
+        flash("Image uploaded and sent for resizing", "success")
+        message = f"/image/{image_dir_name}/{image.filename.split('.')[0]}"
+
+    return render_template('upload_image.html', message=message)
+
+
+@app.route("/image/<dir>/<img>")
+def view_image(dir, img):
+    return render_template("view_image.html", dir=dir, img=img)
